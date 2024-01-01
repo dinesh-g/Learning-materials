4071f6600051076d74b09d5ea17adae1495d6560
dinesh-g <dineshthirumrth@gmail.com>
2024-01-01 13:25:17 +0530

    Add files via upload

---
+++ microservicetutorials/src/test/java/com/learning/microservicetutorials/MicroservicetutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservicetutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicetutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
